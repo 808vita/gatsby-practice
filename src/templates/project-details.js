e74e23ce8d790b73648355914673c6eb0510e267
808vita <its808vita@gmail.com>
2022-10-26 04:03:22 +0530

    deails pagequery| Slug variable using querycontext

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,23 +1,51 @@
+import { graphql } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
-// import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import * as styles from "../styles/project-details.module.css"
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  console.log(data)
+  const { html } = data.markdownRemark
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+
   return (
     <Layout>
       <div className={styles.details}>
-        <h2>title</h2>
-        <h3>stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
         <div className={styles.featured}>
-          {/* <GatsbyImage 
-          image={getImage(
-            project.frontmatter.thumb.childImageSharp.gatsbyImageData
-          )}
-          alt="feature image"
-          /> */}
+          <GatsbyImage
+            image={getImage(featuredImg.childImageSharp.gatsbyImageData)}
+            alt="feature image"
+          />
         </div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+        <div
+          className={styles.html}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectDetail($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData(
+              layout: FULL_WIDTH
+              placeholder: BLURRED
+              formats: [AUTO, WEBP]
+            )
+          }
+        }
+      }
+    }
+  }
+`
+//keep query names as unique
