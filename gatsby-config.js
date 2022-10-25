5da5ac37d2b364e91c5ef7765cab100a353cbc0a
808vita <its808vita@gmail.com>
2022-10-26 00:59:29 +0530

    page queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: "ooF oof",
+    description: "ooof oof oof oof",
+    copyright: "oof oof oof oofoof ooof ooof",
+  },
 }
