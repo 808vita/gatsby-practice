429bbb273052df7b153fa59ec5bc64422464fbce
808vita <its808vita@gmail.com>
2022-10-26 00:40:03 +0530

    project css module

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
@@ -10,6 +11,9 @@ export default function Home() {
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Manchester.</p>
+          <Link to="/projects" className={styles.btn}>
+            My Portfolio Projects.
+          </Link>
         </div>
       </section>
     </Layout>
